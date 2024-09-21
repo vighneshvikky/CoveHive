4c3d894b674b052d234638be92f2f1cf7ac2eec3
vighneshvikky <vighnesh55642@gmail.com>
2024-09-21 09:20:47 +0530

    category schema added

--- models/admin/categoryModel.js
+++ models/admin/categoryModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String },
+  img : {type : String},
   isDeleted: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }  // Used for soft delete
 });
