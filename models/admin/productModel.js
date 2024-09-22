cc9ff1ff74e42d83d12eaaef3cfd27a561ffb5aa
vighneshvikky <vighnesh55642@gmail.com>
2024-09-22 10:37:05 +0530

    product schema added

---
+++ models/admin/productModel.js
@@ -0,0 +1,25 @@
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true }, // Product name
+    description: { type: String }, // Optional product description
+    price: { type: Number, required: true }, // Product price
+    stock: { type: Number, required: true }, // Stock count
+    image: [String], // Array to hold image URLs
+    category: { type: String, required: true }, // e.g., 'Apple', 'Pixel', 'Galaxy'
+    subcategory: { type: String, required: true }, // e.g., 'iPhone 15 Series', 'iPhone 14 Series'
+    compatibleDevices: [String], // e.g., ['iPhone 15 Pro', 'iPhone 15 Pro Max']
+    ratings: { type: Number, default: 0 }, // Optional field for average ratings
+    reviews: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        reviewText: String,
+        rating: Number,
+      },
+    ],
+    isAvailable: { type: Boolean, default: true }, // Availability status
+    discount: { type: Number, default: 0 }, // Discount percentage, if applicable
+    createdAt: { type: Date, default: Date.now },
+    isBlocked: {type:Boolean,default:false}
+  });
+  
+  const Product = mongoose.model('Product', productSchema);
+  
