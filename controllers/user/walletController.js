3d321f73e3c447b1255a2712266324128e07278d
Vighnesh SL <vighnesh55642@gmail.com>
2024-11-12 15:34:31 +0530

    wallet pagination added and sort updated

--- controllers/user/walletController.js
+++ controllers/user/walletController.js
@@ -2,9 +2,12 @@ const Wallet = require('../../models/walletSchema');
 
 
 exports.walletPage = async (req,res)=>{
+    const page = parseInt(req.query.page) || 1; // Get page from query or default to 1
+    const limit =  8;
     try{
+        const walletCount = await Wallet.countDocuments();
         const userId = req.session.user_id;
-        let wallet = await Wallet.findOne({ userID: userId }).sort({transaction_date:-1})
+        let wallet = await Wallet.findOne({ userID: userId }).sort({transaction_date:-1}).skip((page-1)*limit).limit(limit)
         if (!userId) {
             req.flash('error', 'User Not found . Please login again.')
             return res.redirect('/login')
@@ -12,7 +15,12 @@ exports.walletPage = async (req,res)=>{
         if (!wallet) {
             wallet = { balance: 0, transaction: [] };
         }
-        res.render('user/wallet',{title:'Wallet' , wallet})
+        res.render('user/wallet',{title:'Wallet' , wallet,
+
+            currentPage:page,
+            totalPages:Math.ceil(walletCount/limit)
+
+        })
     }catch(error){
         console.log(`error while render user wallet ${error}`)
        
